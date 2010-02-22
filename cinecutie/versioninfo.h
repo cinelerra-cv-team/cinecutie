88c5d00ef097ce64cb6f9fa14a830fff90f012aa
Paolo Rampino <akirad@users.sourceforge.net>
2010-02-22 15:23:54 +0100

    fixed a52 compilation warning\

--- cinecutie/versioninfo.h
+++
@@ -1,4 +0,0 @@
-#define BUILDDATE "ven feb 19 16:11:03 CET 2010"
-#define REPOMAINTXT      "(C) 2006 Heroine Virtual Ltd.\nCompiled on ven feb 19 16:11:03 CET 2010"
-#define REPOABOUTPREFTXT ""
-#define FFMPEG_EXTERNALTEXT "Internal ffmpeg"
