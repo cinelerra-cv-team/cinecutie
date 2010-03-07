bdbe9fd96d14a7780f0c9fd20496266652bfe7b7
Paolo Rampino <akirad@users.sourceforge.net>
2010-03-07 15:42:32 +0100

    fixed speex and openjpeg check

--- cinecutie/versioninfo.h
+++
@@ -1,4 +0,0 @@
-#define BUILDDATE "ven mar  5 02:46:49 CET 2010"
-#define REPOMAINTXT      "(C) 2006 Heroine Virtual Ltd.\nCompiled on ven mar  5 02:46:49 CET 2010"
-#define REPOABOUTPREFTXT ""
-#define FFMPEG_EXTERNALTEXT "Internal ffmpeg"
